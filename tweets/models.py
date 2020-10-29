bc264f6f588d2679a8c2015544b306ecbab869ad
mattc2x4 <mattc2x4@yahoo.com>
2020-10-29 16:31:54 -0400

    formatted and added bootstrap view

--- tweets/models.py
+++ tweets/models.py
@@ -6,3 +6,4 @@ class Tweet(models.Model):
     # id = modles.AutoField(primary_key=True)
     content = models.TextField(blank=True,null=True)
     image = models.FileField(upload_to='images/',blank=True,null=True)
+    # likes = models.IntegerField(blank=True,null=True)
