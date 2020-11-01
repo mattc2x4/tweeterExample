42302e1a9e9e8b5e6d1173162e87b926b941aacd
mattc2x4 <mattc2x4@yahoo.com>
2020-11-01 14:17:09 -0500

    added user auth, error handlign

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Tweet
 
+
+
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'user']
+    search_fields= ['content', 'user__username', 'user__email']
+
+    class Meta:
+        model = Tweet
 # Register your models here.
+admin.site.register(Tweet, TweetAdmin)
+
--- tweets/models.py
+++ tweets/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from django.conf import settings
 import random
 
 # Create your models here. maps django to a database 
 
+User = settings.AUTH_USER_MODEL
+
 class Tweet(models.Model):
     # id = modles.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  #many users have many tweets tweet only has one user, cascade: owner user deleted, all tweets deleted.
     content = models.TextField(blank=True,null=True)
     image = models.FileField(upload_to='images/',blank=True,null=True)
+
     # likes = models.IntegerField(blank=True,null=True)
+
+    def __str__(self):
+        return self.content
 
     class Meta:
         ordering = ['-id']
