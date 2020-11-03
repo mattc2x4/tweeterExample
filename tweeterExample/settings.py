f48a840d794e5b8103f030e95226dba4925c25b7
mattc2x4 <mattc2x4@yahoo.com>
2020-11-03 17:04:54 -0500

    rest settings

--- tweeterExample/settings.py
+++ tweeterExample/settings.py
@@ -42,8 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #third party
+    'rest_framework',
 
+    #internal
     'tweets',
+    
 ]
 
 MIDDLEWARE = [
