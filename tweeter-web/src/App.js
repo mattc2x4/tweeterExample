a8d53bc1f667d2e29fb49e3bd76ae63748f03a04
mattc2x4 <mattc2x4@yahoo.com>
2020-11-12 14:21:31 -0500

    got react to work

--- tweeter-web/src/App.js
+++ tweeter-web/src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  const [tweets, setTweets] = useState([{content: 1234}])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {tweets.map((tweet,index)=>{
+            return <li>{tweet.content}</li>
+          })}
         </p>
         <a
           className="App-link"
