e27029b2e4721e1796d9108f84c641c403c49d59
mattc2x4 <mattc2x4@yahoo.com>
2020-11-12 14:51:03 -0500

    making first request from react

--- tweeter-web/src/App.js
+++ tweeter-web/src/App.js
@@ -2,8 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useState} from 'react';
 
+
+function loadTweets(callback) {
+  const xhr = new XMLHttpRequest()
+  const method = 'GET' // "POST"
+  const url = "http://127.0.0.1:8000/api/tweets"
+  const responseType = "json"
+  xhr.responseType = responseType
+  xhr.open(method, url)
+  xhr.onload = function() {
+    callback(xhr.response,xhr.status)
+  }
+  xhr.onerror = function (e) {
+    callback({"message": "The request was an error"},400)
+  }
+  xhr.send()
+}
+
 function App() {
-  const [tweets, setTweets] = useState([{content: 1234}])
+  
+
+
+  const [tweets, setTweets] = useState([])
+
+  
+
+
+  useEffect(() => {
+    //do lookup
+    const myCallback = (response,status)=>{
+      if (status===200){
+        setTweets(response)
+        console.log(response,status)
+      }
+      else{
+        alert("There was an error")
+      }
+    }
+    loadTweets(myCallback)
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
