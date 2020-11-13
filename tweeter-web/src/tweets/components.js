b7d817802e8d01b0d3faa7e11614a079fb3ff525
mattc2x4 <mattc2x4@yahoo.com>
2020-11-13 15:04:38 -0500

    maintain cleanliness in app.js

--- tweeter-web/src/tweets/components.js
+++ tweeter-web/src/tweets/components.js
@@ -1,20 +1,6 @@
 import React, {useEffect, useState} from 'react';
 
-function loadTweets(callback) {
-    const xhr = new XMLHttpRequest()
-    const method = 'GET' // "POST"
-    const url = "http://localhost:8000/api/tweets"
-    const responseType = "json"
-    xhr.responseType = responseType
-    xhr.open(method, url)
-    xhr.onload = function() {
-      callback(xhr.response,xhr.status)
-    }
-    xhr.onerror = function (e) {
-      callback({"message": "The request was an error"},400)
-    }
-    xhr.send()
-  }
+import {loadTweets} from '../lookup'
   
 export function TweetsList(props){
     const [tweets, setTweets] = useState([])
