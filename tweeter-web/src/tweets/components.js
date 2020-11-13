3592b8bce011454ba67b9c9367a1b757d158fc50
mattc2x4 <mattc2x4@yahoo.com>
2020-11-13 17:25:49 -0500

    submitting tweets works on frontend

--- tweeter-web/src/tweets/components.js
+++ tweeter-web/src/tweets/components.js
@@ -4,13 +4,16 @@ import { loadTweets } from '../lookup'
 
 export function TweetsComponent(props) {
     const textAreaRef = React.createRef()
-
+    const [newTweets, setNewTweets] = useState([])
     const handleSubmit = (event) => {
         event.preventDefault()
-        console.log(event)
-        console.log(textAreaRef.current.value)
-        textAreaRef.current.value = ''
         const newVal = textAreaRef.current.value
+        let tempNewTweets = [...newTweets]
+        tempNewTweets.unshift({
+            content: newVal,
+        })
+        setNewTweets(tempNewTweets)
+        textAreaRef.current.value = ''
     }
     return <div className={props.className}>
         <div className='col-12 mb-3'>
@@ -21,19 +24,28 @@ export function TweetsComponent(props) {
                 <button type='submit' className='btn btn-primary my-3'>Tweet</button>
             </form>
         </div>
-        <TweetsList/>
+        <TweetsList newTweets={newTweets}/>
     </div>
 }
 
 
 
 export function TweetsList(props) {
+    const [tweetsInit, setTweetsInit] = useState([])
     const [tweets, setTweets] = useState([])
+    // setTweetsInit([...props.newTweets].concat(tweetsInit))
+    useEffect(()=> {
+        const final = [...props.newTweets].concat(tweetsInit)
+        if (final.length!==tweets.length){
+            setTweets(final)
+        }
+    }, [props.newTweets,tweets,tweetsInit])
+
     useEffect(() => {
         //do lookup
         const myCallback = (response, status) => {
             if (status === 200) {
-                setTweets(response)
+                setTweetsInit(response)
             }
             else {
                 alert("There was an error")
