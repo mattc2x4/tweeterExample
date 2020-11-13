241112bad205e65cb8cdcccd4e7ca7ac72185cdd
mattc2x4 <mattc2x4@yahoo.com>
2020-11-13 14:59:57 -0500

    react actionbtn, reorgainzed files

--- tweeter-web/src/App.js
+++ tweeter-web/src/App.js
@@ -2,45 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useState} from 'react';
 
+import {TweetsList} from './tweets'
 
-function loadTweets(callback) {
-  const xhr = new XMLHttpRequest()
-  const method = 'GET' // "POST"
-  const url = "http://localhost:8000/api/tweets"
-  const responseType = "json"
-  xhr.responseType = responseType
-  xhr.open(method, url)
-  xhr.onload = function() {
-    callback(xhr.response,xhr.status)
-  }
-  xhr.onerror = function (e) {
-    callback({"message": "The request was an error"},400)
-  }
-  xhr.send()
-}
+
+
+
 
 function App() {
   
-
-
-  const [tweets, setTweets] = useState([])
-
-  
-
-
-  useEffect(() => {
-    //do lookup
-    const myCallback = (response,status)=>{
-      if (status===200){
-        setTweets(response)
-        console.log(response,status)
-      }
-      else{
-        alert("There was an error")
-      }
-    }
-    loadTweets(myCallback)
-  }, [])
 
 
   return (
@@ -50,11 +19,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          {tweets.map((tweet,index)=>{
-            return <li>{tweet.content}</li>
-          })}
-        </p>
+        <div>
+          <TweetsList/>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
