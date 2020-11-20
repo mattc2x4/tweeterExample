42b5d70e0363c542344db751ac120813203f4976
mattc2x4 <mattc2x4@yahoo.com>
2020-11-20 15:07:11 -0500

    removed actions from retweet

--- tweeter-web/src/tweets/components.js
+++ tweeter-web/src/tweets/components.js
@@ -112,13 +112,13 @@ export function ParentTweet(props) {
     return tweet.parent ? <div className='row'>
         <div className='col-11 mx-auto p-3 border rounded'>
             <p className='mb-0 text-muted small'>Retweet</p>
-            <Tweet className={' '} tweet={tweet.parent} />
+            <Tweet hideActions className={' '} tweet={tweet.parent} />
         </div>
     </div> : null
 }
 
 export function Tweet(props) {
-    const { tweet, didRetweet } = props
+    const { tweet, didRetweet, hideActions } = props
     const [actionTweet, setActionTweet] = useState(props.tweet ? props.tweet : null)
     const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
 
@@ -139,7 +139,7 @@ export function Tweet(props) {
             <p>{tweet.id} - {tweet.content}</p>
             <ParentTweet tweet={tweet} />
         </div>
-        {actionTweet && <div className='btn btn-group'>
+        {(actionTweet && hideActions !== true) && <div className='btn btn-group'>
             <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{ type: "like", display: "Likes" }} />
             <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{ type: "unlike", display: "Unlike" }} />
             <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{ type: "retweet", display: "Retweet" }} />
