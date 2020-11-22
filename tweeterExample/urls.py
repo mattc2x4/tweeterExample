c6ba22a729a5d710c70310a2e1cc51569323f894
mattc2x4 <mattc2x4@yahoo.com>
2020-11-22 17:09:10 -0500

    migrated to django smh

--- tweeterExample/urls.py
+++ tweeterExample/urls.py
@@ -21,21 +21,19 @@ from django.views.generic import TemplateView
 
 from tweets.views import home_view, tweet_action_view
 from tweets.views import (
-    home_view, tweet_detail_view, tweet_list_view, tweet_create_view, tweet_delete_view,
+    local_tweet_list_view,
+    local_tweet_detail_view,
+    local_tweet_profile_view
 ) 
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view),
-    path('react/', TemplateView.as_view(template_name='react.html')),
-    path('create-tweet', tweet_create_view),
-    path('tweets', tweet_list_view),
-    # path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
-    # path('api/tweets/action', tweet_action_view),
-    path('tweets/<int:tweet_id>/', tweet_detail_view),
-    path('api/tweets/', include('tweets.urls'))
+    path('', local_tweet_list_view),
+    path('<int:tweet_id>', local_tweet_detail_view),
+    path('profile/<str:username>', local_tweet_profile_view),
+    path('api/tweets/', include('tweets.urls')),
 ]
 
 if settings.DEBUG:
--- tweets/views.py
+++ tweets/views.py
@@ -35,6 +35,15 @@ def home_view(request, *args, **kwargs):
         username = request.user.username
     return render(request, "pages/home.html", context={"username": username}, status=200)
 
+def local_tweet_list_view(request, *args, **kwargs):
+    return render(request, "tweets/list.html")
+
+def local_tweet_detail_view(request, tweet_id, *args, **kwargs):
+    return render(request, "tweets/detail.html", context={"tweet_id": tweet_id})
+
+def local_tweet_profile_view(request, username, *args, **kwargs):
+    return render(request, "tweets/profile.html", context={"profile_username": username}, status=200)
+
 
 @api_view(['POST'])  # http method the client == POST
 # @authentication_classes([SessionAuthentication, MyCustomAuth])
