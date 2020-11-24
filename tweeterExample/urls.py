46d852ea17824dcb0d83de79bfa564efc1f1a943
mattc2x4 <mattc2x4@yahoo.com>
2020-11-24 13:04:13 -0500

    api migrated

--- tweeterExample/urls.py
+++ tweeterExample/urls.py
@@ -13,29 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, re_path, include # url()
 from django.views.generic import TemplateView
 
-from tweets.views import home_view, tweet_action_view
 from tweets.views import (
-    local_tweet_list_view,
-    local_tweet_detail_view,
-    local_tweet_profile_view
-) 
-
-
+    tweets_list_view,
+    tweets_detail_view,
+    tweets_profile_view,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', local_tweet_list_view),
-    path('<int:tweet_id>', local_tweet_detail_view),
-    path('profile/<str:username>', local_tweet_profile_view),
-    path('api/tweets/', include('tweets.urls')),
+    path('', tweets_list_view),
+    path('<int:tweet_id>', tweets_detail_view),
+    path('profile/<str:username>', tweets_profile_view),
+    path('api/tweets/', include('tweets.api.urls'))
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-    document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, 
+                document_root=settings.STATIC_ROOT)
